4581e39f8d2dc215a84c5a8f6194a5ee1f9f08d2
mtumalan <mau@tumalan.com>
2025-06-03 21:15:30 -0600

    serializer change

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -38,6 +38,8 @@ CORS_ALLOWED_ORIGINS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
